f289e81cdf52cdb72dee01457c50da22dea40269
samckp <sam.mca.liet@gmail.com>
2019-08-18 12:37:52 +0530

    code updated

--- src/main/java/com/sam/camel/route/TimerRoute.java
+++ src/main/java/com/sam/camel/route/TimerRoute.java
@@ -5,15 +5,15 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SimpleRoute extends RouteBuilder {
+public class TimerRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
-        from("{{fromRoute}}")
-                .routeId("fileMoveRouteId")
+        from("{{timeRoute}}")
+                .routeId("timerRoute")
+                .setBody(simple("this is test msg ! ------{{message}}"))
                 .log(LoggingLevel.INFO, "${body}")
-            .to("{{toRoute}}")
-         ;
+                .to("mock:out");
     }
 }
