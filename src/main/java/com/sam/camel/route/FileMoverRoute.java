77f64219277492f417b2676c15ec1776ad1c5284
samckp <sam.mca.liet@gmail.com>
2020-03-24 08:50:48 +0530

    updated

---
+++ src/main/java/com/sam/camel/route/FileMoverRoute.java
@@ -0,0 +1,20 @@
+package com.sam.camel.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileMoverRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("{{fromRoute}}")
+                .routeId("FileMoverRoute")
+//                .log(LoggingLevel.INFO, "${body}")
+                .process(new FileProcessor())
+                .to("mock:end")
+            ;
+    }
+}
---
+++ src/main/java/com/sam/camel/route/FileProcessor.java
@@ -0,0 +1,17 @@
+package com.sam.camel.route;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import java.io.File;
+
+public class FileProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+//        String file = exchange.getIn().getBody(String.class);
+        byte[] bytes =exchange.getIn().getBody(byte[].class);
+        System.out.println(bytes);
+    }
+}
