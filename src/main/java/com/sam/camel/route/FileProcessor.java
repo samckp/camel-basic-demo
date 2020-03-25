10d0c0f1c64260edde0bdaea8f30749e500167af
samckp <sam.mca.liet@gmail.com>
2020-03-25 08:59:21 +0530

    updated

--- src/main/java/com/sam/camel/route/FileProcessor.java
+++ src/main/java/com/sam/camel/route/FileProcessor.java
@@ -10,8 +10,8 @@ public class FileProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
 
-//        String file = exchange.getIn().getBody(String.class);
-        byte[] bytes =exchange.getIn().getBody(byte[].class);
-        System.out.println(bytes);
+        String file = exchange.getIn().getBody(String.class);
+//        byte[] bytes =exchange.getIn().getBody(byte[].class);
+        System.out.println(file);   // print the contents of file.
     }
 }
